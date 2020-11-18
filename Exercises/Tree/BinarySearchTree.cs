aafdae6fa8572a79d59ed313eba8ce407e5f15bf
Peter <peterrose03@gmail.com>
2020-11-18 16:55:45 +0100

    Added BinarySearchTree

---
+++ Exercises/Tree/BinarySearchTree.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Exercises
+{
+    public class BinarySearchTree
+    {
+        private TreeNode root;
+
+        public BinarySearchTree()
+        {
+            this.root = null;
+        }
+
+        public void Insert(int value)
+        {
+            var newTreeNode = new TreeNode(value);
+
+            if (this.root == null)
+            {
+                this.root = newTreeNode;
+            }
+            else
+            {
+                var currentNode = this.root;
+
+                while (true)
+                {
+                    if(value < currentNode.Value)
+                    {
+                        //Left
+                        if (currentNode.Left == null)
+                        {
+                            currentNode.Left = newTreeNode;
+                            return;
+                        }
+
+                        currentNode = currentNode.Left;
+                    }
+                    else
+                    {
+                        //Right
+                        if(currentNode.Right == null)
+                        {
+                            currentNode.Right = newTreeNode;
+                            return;
+                        }
+
+                        currentNode = currentNode.Right;
+                    }
+                }
+            }
+        }
+
+        public bool Lookup(int value)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            var currentNode = this.root;
+
+            while (currentNode != null)
+            {
+                if (value == currentNode.Value)
+                {
+                    return true;
+                }
+                else if (value < currentNode.Value)
+                {
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    currentNode = currentNode.Right;
+                }
+            }
+
+            return false;
+        }
+
+        public void Remove(int value)
+        {
+        }
+
+        private TreeNode TraversTreeNodes(TreeNode node)
+        {
+            var treeNode = new TreeNode(node.Value);
+            treeNode.Right = node.Right == null ? null : this.TraversTreeNodes(node.Right);
+            treeNode.Left = node.Left == null ? null : this.TraversTreeNodes(node.Left);
+
+            return treeNode;
+        }
+    }
+
+    public class TreeNode
+    {
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+        public int Value { get; set; }
+
+        public TreeNode(int value)
+        {
+            this.Left = null;
+            this.Right = null;
+            this.Value = value;
+        }
+    }
+}
---
+++ NUnitTests/Tree/BinarySearchTreeTests.cs
@@ -0,0 +1,49 @@
+﻿using Exercises;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NUnitTests.Tree
+{
+    public class BinarySearchTreeTests
+    {
+        // This is the tree we use in these tests.
+        //           9
+        //    4             20
+        //  1   6       15      170
+
+        [TestCase(new int[] { 9, 4, 20, 1, 6, 15, 170 })]
+        public void TestingBinarySearchTreeInsert(int[] arr)
+        {
+            //var Expected = new List<int>() { 9, 4, 20, 1, 6, 15, 170 };
+
+            var binarySearchTree = new BinarySearchTree();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                binarySearchTree.Insert(arr[i]);
+            }
+
+            //var result = binarySearchTree.PrintLinkedList();
+
+            //Assert.AreEqual(Expected, null);
+        }
+
+        [TestCase(new int[] { 9, 4, 20, 1, 6, 15, 170 }, 1, true)]
+        [TestCase(new int[] { 9, 4, 20, 1, 6, 15, 170 }, 99, false)]
+        public void TestingBinarySearchTreeLookup(int[] arr, int lookup, bool expected)
+        {
+            var Expected = new List<int>() { 10, 5, 16 };
+
+            var binarySearchTree = new BinarySearchTree();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                binarySearchTree.Insert(arr[i]);
+            }
+
+            var result = binarySearchTree.Lookup(lookup);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
