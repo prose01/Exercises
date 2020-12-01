e935101db9407770725856fddd26dbff76498856
Peter <peterrose03@gmail.com>
2020-12-01 14:04:45 +0100

    Added BubbleSort

---
+++ Exercises/Sorting/BubbleSort.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercises
+{
+    public class BubbleSort
+    {
+        // Not a very good sort Θ(n^2) but at least it does not use any extra space, O(1).
+        public int[] Sort(int[] array)
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                for (var n = 0; n < array.Length - 1; n++)
+                {
+                    if (array[n] > array[n + 1])
+                    {
+                        // Swap numbers
+                        var temp = array[n];
+
+                        array[n] = array[n + 1];
+                        array[n + 1] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
---
+++ NUnitTests/Sorting/BubbleSortTests.cs
@@ -0,0 +1,19 @@
+﻿using Exercises;
+using NUnit.Framework;
+
+namespace NUnitTests.Sorting
+{
+    public class BubbleSortTests
+    {
+        [TestCase(new int[] { 9, 4, 20, 1, 6, 15, 170 }, new int[] { 1, 4, 6, 9, 15, 20, 170 })]
+        [TestCase(new int[] { 2, 4, 1, 3 }, new int[] { 1, 2, 3, 4 })]
+        public void TestingBinarySearchTreeLookup(int[] arr, int[] expected)
+        {
+            var bubbleSort = new BubbleSort();
+
+            var result = bubbleSort.Sort(arr);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
