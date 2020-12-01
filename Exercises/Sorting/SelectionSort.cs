5d88799cd793558e20ad4d56f6125e4550bb91c1
Peter <peterrose03@gmail.com>
2020-12-01 15:40:11 +0100

    Added SelectionSort

---
+++ Exercises/Sorting/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercises
+{
+    public class SelectionSort
+    {
+        // Not a very good sort Θ(n^2) but at least it does not use any extra space, O(1).
+        public int[] Sort(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                // Set current index as samllest.
+                var tempIndex = i;
+                var smallest = array[i];
+
+                for (var n = i + 1; n < array.Length; n++)
+                {
+                    if (array[tempIndex] > array[n])
+                    {
+                        // Update samllest if current is  lower.
+                        tempIndex = n;
+                    }
+                }
+
+                array[i] = array[tempIndex];
+                array[tempIndex] = smallest;
+            }
+
+            return array;
+        }
+    }
+}
--- NUnitTests/Sorting/SelectionSortTests.cs
+++ NUnitTests/Sorting/SelectionSortTests.cs
@@ -3,15 +3,15 @@ using NUnit.Framework;
 
 namespace NUnitTests.Sorting
 {
-    public class BubbleSortTests
+    public class SelectionSortTests
     {
         [TestCase(new int[] { 9, 4, 20, 1, 6, 15, 170 }, new int[] { 1, 4, 6, 9, 15, 20, 170 })]
         [TestCase(new int[] { 2, 4, 1, 3 }, new int[] { 1, 2, 3, 4 })]
-        public void TestingBubbleSortT(int[] arr, int[] expected)
+        public void TestingSelectionSort(int[] arr, int[] expected)
         {
-            var bubbleSort = new BubbleSort();
+            var selectionSort = new SelectionSort();
 
-            var result = bubbleSort.Sort(arr);
+            var result = selectionSort.Sort(arr);
 
             Assert.AreEqual(expected, result);
         }
