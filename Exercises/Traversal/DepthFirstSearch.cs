16cf8a510c89d53b178faeff2201882a4a429b8e
Peter <peterrose03@gmail.com>
2020-12-08 11:33:51 +0100

    Added DepthFirstSearch

---
+++ Exercises/Traversal/DepthFirstSearch.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace Exercises
+{
+    public class DepthFirstSearch
+    {
+        // Recursive function. 
+        public List<int> DFSInOrder(TreeNode node, List<int> list)
+        {
+            if(node.Left != null)
+            {
+                DFSInOrder(node.Left, list);
+            }
+
+            //In Order add is between Left and Right.
+            list.Add(node.Value);
+
+            if (node.Right != null)
+            {
+                DFSInOrder(node.Right, list);
+            }
+
+            return list;
+        }
+
+        // Recursive function. 
+        public List<int> DFSPreOrder(TreeNode node, List<int> list)
+        {
+            //Pre Order add is before Left and Right.
+            list.Add(node.Value);
+
+            if (node.Left != null)
+            {
+                DFSPreOrder(node.Left, list);
+            }
+
+            if (node.Right != null)
+            {
+                DFSPreOrder(node.Right, list);
+            }
+
+            return list;
+        }
+
+        // Recursive function. 
+        public List<int> DFSPostOrder(TreeNode node, List<int> list)
+        {
+            if (node.Left != null)
+            {
+                DFSPostOrder(node.Left, list);
+            }
+
+            if (node.Right != null)
+            {
+                DFSPostOrder(node.Right, list);
+            }
+
+            //Post Order add is after Left and Right.
+            list.Add(node.Value);
+
+            return list;
+        }
+    }
+}
---
+++ NUnitTests/Traversal/DepthFirstSearchTests.cs
@@ -0,0 +1,65 @@
+﻿using Exercises;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NUnitTests.Traversal
+{
+    public class DepthFirstSearchTests
+    {
+        // This is the tree we use in these tests.
+        //           9
+        //    4             20
+        //  1   6       15      170
+
+        [TestCase(new int[] { 9, 4, 20, 1, 6, 15, 170 }, new int[] { 1, 4, 6, 9, 15, 20, 170 })]
+        public void TestingDFSInOrder(int[] arr, int[] expected)
+        {
+            var binarySearchTree = new BinarySearchTree();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                binarySearchTree.Insert(arr[i]);
+            }
+
+            List<int> list = new List<int>();
+            var depthFirstSearch = new DepthFirstSearch();
+            var result = depthFirstSearch.DFSInOrder(binarySearchTree.root, list);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(new int[] { 9, 4, 20, 1, 6, 15, 170 }, new int[] { 9, 4, 1, 6, 20, 15, 170 })]
+        public void TestingDFSPreOrder(int[] arr, int[] expected)
+        {
+            var binarySearchTree = new BinarySearchTree();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                binarySearchTree.Insert(arr[i]);
+            }
+
+            List<int> list = new List<int>();
+            var depthFirstSearch = new DepthFirstSearch();
+            var result = depthFirstSearch.DFSPreOrder(binarySearchTree.root, list);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(new int[] { 9, 4, 20, 1, 6, 15, 170 }, new int[] { 1, 6, 4, 15, 170, 20, 9 })]
+        public void TestingDFSPostOrder(int[] arr, int[] expected)
+        {
+            var binarySearchTree = new BinarySearchTree();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                binarySearchTree.Insert(arr[i]);
+            }
+
+            List<int> list = new List<int>();
+            var depthFirstSearch = new DepthFirstSearch();
+            var result = depthFirstSearch.DFSPostOrder(binarySearchTree.root, list);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
