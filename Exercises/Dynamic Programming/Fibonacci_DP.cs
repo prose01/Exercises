345ccc042588684b9c79d4ea65836c6c00da82c3
Peter <peterrose03@gmail.com>
2020-12-11 13:33:36 +0100

    Added Dynamic Programming - Fibonacci_DP

---
+++ Exercises/Dynamic Programming/Fibonacci_DP.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises
+{
+    public class Fibonacci_DP
+    {
+        // Given a number N return the index value of the Fibonacci sequence, where the sequence is:
+
+        // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+        // the pattern of the sequence is that each value is the sum of the 2 previous values, that means that for N=5 → 2+3
+
+        //For example: fibonacciRecursive(6) should return 8
+
+        //public Func<int, int> Fibonacci()
+        //{
+        //    Dictionary<int, int> memory = new Dictionary<int, int>();
+
+        //    Func<int, int> FibonacciRecursively = delegate (int n)
+        //    {
+        //        if (n < 2)
+        //        {
+        //            return n;
+        //        }
+
+        //        if (memory.ContainsKey(n))
+        //        {
+        //            return memory[n];
+        //        }
+
+        //        memory.Add(n, this.FibonacciRecursively(n - 1) + this.FibonacciRecursively(n - 2));
+
+        //        return memory[n];
+        //    };
+
+        //    return FibonacciRecursively;
+        //}
+
+        private Dictionary<int, int> cache = new Dictionary<int, int>();
+
+        public int FibonacciMaster(int n)
+        {
+            if (cache.ContainsKey(n))
+            {
+                return cache[n];
+            }
+            
+            if (n < 2)
+            {
+                return n;
+            }
+            
+            cache.Add(n, FibonacciMaster(n - 1) + FibonacciMaster(n - 2));
+            
+            return cache[n];
+        }
+
+        //Builds up a list from buttom up instead of doing recursion. It keeps adding to the memory list as it goes along. 
+        public int FibonacciButtomUp(int n)
+        {
+            List<int> memory = new List<int>() { 0, 1 };
+
+            for(var i = 2; i <= n; i++) // Remember only to run up until current n number. 
+            {
+                memory.Add(memory[i-1] + memory[i-2]); // Take the two preceeding numbers and add them together.
+            }
+
+            return memory[memory.Count-1]; // return the last added number. 
+        }
+    }
+}
---
+++ NUnitTests/Dynamic Programming Tests/Fibonacci_DPTests.cs
@@ -0,0 +1,34 @@
+﻿using Exercises;
+using NUnit.Framework;
+
+namespace NUnitTests.Dynamic_Programming_Tests
+{
+    public class Fibonacci_DPTests
+    {
+        [TestCase(2, 1)]
+        [TestCase(6, 8)]
+        [TestCase(8, 21)]
+        [TestCase(12, 144)]
+        public void TestingFibonacciMaster(int number, int expected)
+        {
+            var fibonacci_DP = new Fibonacci_DP();
+
+            var result = fibonacci_DP.FibonacciMaster(number);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(2, 1)]
+        [TestCase(6, 8)]
+        [TestCase(8, 21)]
+        [TestCase(12, 144)]
+        public void TestingFibonacciButtomUp(int number, int expected)
+        {
+            var fibonacci_DP = new Fibonacci_DP();
+
+            var result = fibonacci_DP.FibonacciButtomUp(number);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
