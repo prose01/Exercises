1622ea2b9697f05cbcf0f0340339c586059090df
Peter <peterrose03@gmail.com>
2020-12-12 22:18:11 +0100

    Added SubArraySum

---
+++ Exercises/Extra/SubArraySum.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Exercises
+{
+    public class SubArraySum
+    {
+        public int MaxSubArraySum(int[] arr, int num) // 0(n^2) My Naive approach
+        {
+            if(arr.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+
+            for(var i = 0; i < arr.Length; i++)
+            {
+                var subSum = arr[i];
+
+                for(var j = 1; j < num; j++)
+                {
+                    if (i + num < arr.Length)
+                    {
+                        subSum += arr[i + j];
+                    }
+                }
+
+                sum = Math.Max(sum, subSum);
+                //if (sum < subSum)
+                //{
+                //    sum = subSum;
+                //}
+            }
+
+            return sum;
+        }
+
+        public int MaxSubArraySum2(int[] arr, int num) // 0(n^2) Teachers Naive approach. 
+        {
+            if (arr.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+
+            for (var i = 0; i < arr.Length - num + 1; i++) // Bedre - Dette sikre at vi ikke kommer forbi arr.Lenth.
+            {
+                var subSum = 0;
+
+                for (var j = 0; j < num; j++)
+                {
+                    subSum += arr[i + j];
+                }
+
+                if (sum < subSum)
+                {
+                    sum = subSum;
+                }
+            }
+
+            return sum;
+        }
+
+        public int MaxSubArraySumSlidingWindow(int[] arr, int num) // 0(n) Teachers. 
+        {
+            int maxSum = 0;
+            int subSum = 0;
+
+            if (arr.Length < num)
+            {
+                return 0;
+            }
+
+            for (var i = 0; i < num; i++)   // Just add up the first num of digits to create your window.
+            {
+                maxSum += arr[i];
+            }
+
+            subSum = maxSum;
+
+            for (var i = num; i < arr.Length; i++)      // Now move the window by subtracting and adding.
+            {
+                subSum = subSum - arr[i - num] + arr[i]; // Subtract the first/left (arr[i - num]) and add the last/right (arr[i]) of your window.
+
+                maxSum = Math.Max(maxSum, subSum); // Check which is bigger and keep that.
+            }
+            
+            return maxSum;
+        }
+    }
+}
---
+++ NUnitTests/Extra/SubArraySumTests.cs
@@ -0,0 +1,36 @@
+﻿using Exercises;
+using NUnit.Framework;
+
+namespace NUnitTests.Extra
+{
+    public class SubArraySumTests
+    {
+        [TestCase(new int[] { 1, 2, 5, 2, 8, 1, 5 }, 2, 10)]
+        [TestCase(new int[] { 1, 2, 5, 2, 8, 1, 5 }, 4, 17)]
+        [TestCase(new int[] { 4, 2, 1, 6 }, 1, 6)]
+        [TestCase(new int[] { 4, 2, 1, 6, 2 }, 4, 13)]
+        [TestCase(new int[] { }, 4, 0)]
+        public void TestingMaxSubArraySum(int[] arr, int n, int expected)
+        {
+            var subArraySum = new SubArraySum();
+
+            var result = subArraySum.MaxSubArraySum2(arr, n);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(new int[] { 1, 2, 5, 2, 8, 1, 5 }, 2, 10)]
+        [TestCase(new int[] { 1, 2, 5, 2, 8, 1, 5 }, 4, 17)]
+        [TestCase(new int[] { 4, 2, 1, 6 }, 1, 6)]
+        [TestCase(new int[] { 4, 2, 1, 6, 2 }, 4, 13)]
+        [TestCase(new int[] { }, 4, 0)]
+        public void TestingMaxSubArraySumSlidingWindow(int[] arr, int n, int expected)
+        {
+            var subArraySum = new SubArraySum();
+
+            var result = subArraySum.MaxSubArraySumSlidingWindow(arr, n);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
